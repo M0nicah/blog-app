7a8a80977feb49c60691e16d7f939c8aace26d51
M0nicah <monicawughanga@gmail.com>
2022-05-15 11:44:28 +0300

    initialized app in init.py, add test and configured manage.py

---
+++ manage.py
@@ -0,0 +1,22 @@
+from app import createapp, db
+from app.models import User, Blog
+
+# Creating app instance
+app = createapp()
+
+
+# @app.test_request_context()
+def test():
+    """Run the unit tests"""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Blog=Blog)
+
+
+if __name__ == '__main__':
+    app.run()
